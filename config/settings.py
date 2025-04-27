061561a4e40a587cd80ac002903c4131c21bfd43
arim-ryou <79498334+arim-ryou@users.noreply.github.com>
2025-04-27 17:40:43 +0900

    ip

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%(m4+u+7qh8-vf=04@jx3)=kz)$62wmnkx3*_%ppn(otje832m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.37.58.70']
+ALLOWED_HOSTS = ['15.164.132.222']
 
 
 # Application definition
